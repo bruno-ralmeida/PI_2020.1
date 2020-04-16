7a7e68b856b184b25ce914c722dbfb68f470401a
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-04-16 13:00:18 -0300

    Configurando novo diretório de fotos do perfil.

--- app/atendente/models.py
+++ app/atendente/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import time
 
 # Create your models here.
 class Atendente(models.Model):
@@ -10,4 +11,4 @@ class Atendente(models.Model):
     cpf = models.CharField(max_length=20, unique=True)
     rg = models.CharField(max_length=20, unique=True)
     email = models.EmailField()
-    foto = models.ImageField(upload_to='fotos/', blank=True)
+    foto = models.ImageField(upload_to=f'fotos/{time.time()}', blank=True)
--- app/medico/models.py
+++ app/medico/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import time
 
 # Create your models here.
 class Medico(models.Model):
@@ -11,5 +12,5 @@ class Medico(models.Model):
     cpf = models.CharField(max_length=20, unique=True)
     rg = models.CharField(max_length=20, unique=True)
     email = models.EmailField()
-    foto = models.ImageField(upload_to='fotos/', blank=True)
+    foto = models.ImageField(upload_to=f'fotos/{time.time()}', blank=True)
     
